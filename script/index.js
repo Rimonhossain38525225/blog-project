a9de83466aa3948d2f9a9545882c57ce17013e9d
Rimon Hossain <121511488+Rimonhossain38525225@users.noreply.github.com>
2023-01-21 13:03:52 +0600

    HTML, CSS, and javascript file

---
+++ script/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+
+menuIcon.addEventListener("click", ()=> {
+    if (menu.className === 'hidden') {
+        menu.classList.remove('hidden');
+    }else{
+        menu.classList.add('hidden');
+    }
+});
